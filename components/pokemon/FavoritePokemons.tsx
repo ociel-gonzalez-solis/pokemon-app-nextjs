b56756243327942e12b302e03ca6e777b69f2ad2
Ociel Gonzalez Solis <ociel.gonzalez@kiubix.com>
2023-05-17 22:02:49 -0700

    Favorite section done.

---
+++ components/pokemon/FavoritePokemons.tsx
@@ -0,0 +1,15 @@
+import { Grid } from "@nextui-org/react";
+import { FC } from "react";
+import { FavoriteCardPokemon } from "./FavoriteCardPokemon";
+
+interface Props {
+  pokemons: number[];
+}
+
+export const FavoritePokemons: FC<Props> = ({ pokemons }) => (
+  <Grid.Container gap={2} direction="row" justify="flex-start">
+    {pokemons.map((id) => (
+      <FavoriteCardPokemon key={id} pokemonId={id} />
+    ))}
+  </Grid.Container>
+);
